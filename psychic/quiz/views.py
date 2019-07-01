a4afbe82b85c923b521b3e013eb15aab2a34c688
Wesley Soo-Hoo <wesoohoo@gmail.com>
2019-07-01 05:36:52 -0700

    (#13) First pass at logging
    Log quiz submissions as json files. Note that right now it will override
    the previous log if the same person submits twice-- the log file name is
    the user's name

--- psychic/quiz/views.py
+++ psychic/quiz/views.py
@@ -3,10 +3,14 @@ import json
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.http import HttpResponse, Http404
+from django.utils import timezone
 from dicttoxml import dicttoxml
 
 from .models import Quiz, Question, Choice, Prediction
-from .settings import DEBUG
+from . import settings
+
+import os
+import pathlib
 
 
 class IndexView(generic.ListView):
@@ -28,7 +32,7 @@ class QuizView(generic.DetailView):
 
 def quiz(request, pk):
     selected_quiz = get_object_or_404(Quiz, pk=pk)
-    if (selected_quiz.is_valid() and selected_quiz.is_active()) or DEBUG:
+    if (selected_quiz.is_valid() and selected_quiz.is_active()) or settings.DEBUG:
         context = {
             'quiz': selected_quiz,
         }
@@ -37,16 +41,23 @@ def quiz(request, pk):
         raise Http404("Quiz does not exist!")
 
 
-
-def submit(request, quiz_id, question_format="question{}"):
+def submit(request, quiz_id, question_format="question{}", save_path=settings.LOG_DIR):
     quiz = get_object_or_404(Quiz, pk=quiz_id)
     quiz_results = {}
 
+    quiz_results['time_submitted'] = timezone.now().isoformat()
+
     if 'name' in request.POST.keys():
         quiz_results['name'] = request.POST['name']
+    else:
+        # TODO throw error for unfinished quiz
+        pass
 
     if 'email' in request.POST.keys():
         quiz_results['email'] = request.POST['email']
+    else:
+        # TODO throw error for unfinished quiz
+        pass
 
     prediction_set = {}
 
@@ -71,6 +82,18 @@ def submit(request, quiz_id, question_format="question{}"):
     quiz_results['prediction'] = final_prediction.prediction_title
 
     # Collect client data
+    quiz_results['client_data'] = _get_client_data()
+
+    # Save data
+    log_subdir = str(quiz.pk)
+    filename = "{}.json".format(request.POST['name'].replace(" ", ""))
+    _log_json(save_path, log_subdir, filename)
+
+    return HttpResponse(json.dumps(quiz_results))
+
+
+def _get_client_data(request):
+    # Collect client data
     client_data = {
         'server_name': request.META['SERVER_NAME'],
         'server_port': request.META['SERVER_PORT'],
@@ -79,7 +102,16 @@ def submit(request, quiz_id, question_format="question{}"):
         'clinet_ip': request.META['REMOTE_ADDR'],
     }
 
-    quiz_results['client_data'] = client_data
+    return client_data
 
-    return HttpResponse(json.dumps(quiz_results))
+
+def _log_json(save_path, log_subdir, filename):
+    # Save data
+    logdir = os.path.join(save_path, log_subdir)
+    if not os.path.exists(save_path):
+        pathlib.Path(logdir).mkdir(parents=True, exist_ok=True)
+
+    # Write data
+    with open(os.path.join(logdir, filename), 'w') as outfile:
+        json.dump(quiz_results, outfile)
 
