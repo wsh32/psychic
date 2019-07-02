75f5a08b5e2a891ca44e7d023d99dd6590e0745a
Wesley Soo-Hoo <wesoohoo@gmail.com>
2019-07-02 01:18:17 -0700

    (#12) Modify admin page to simplify quiz input
    Quiz input is done in two steps now, quiz creation (which also allows
    input of questions and predictions) and question editing (where the
    choices are added in). This is still a little clunky but a major
    improvement over what it was before.

--- psychic/quiz/admin.py
+++ psychic/quiz/admin.py
@@ -3,9 +3,38 @@ from django.contrib import admin
 from .models import Quiz, Question, Choice, Prediction, Submission
 
 
-admin.site.register(Quiz)
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Prediction)
+class PredictionInLine(admin.TabularInline):
+    model = Prediction
+    fields = ['prediction_title']
+    extra = 4
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    fields = ['choice_text', 'question', 'prediction', 'weight']
+    extra = 4
+
+
+class QuestionInLine(admin.TabularInline):
+    model = Question
+    fields = ['question_text']
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['quiz', 'question_text']
+    inlines = [ChoiceInLine]
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Date Information', {'fields': ['pub_date', 'exp_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [PredictionInLine, QuestionInLine]
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Submission)
 
